aa92249850435d002a7cd0e5a7432c8db2a8bdc2
hasegawa kei <yahooku.kei@gmail.com>
2019-12-09 15:48:05 +0900

    upload修正3

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -4,10 +4,10 @@ CKEDITOR.editorConfig = function(config) {
   config.filebrowserFlashBrowseUrl = "/ckeditor/attachment_files";
   config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
   config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
-  config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
-  config.filebrowserImageUploadUrl = "/ckeditor/pictures";
+  config.filebrowserImageBrowseUrl = "/ckeditor/pictures?";
+  config.filebrowserImageUploadUrl = "/ckeditor/pictures?";
   config.filebrowserUploadUrl = "/ckeditor/attachment_files";
-  config.filebrowserImageUploadUrl = "/ckeditor/pictures?=true";
+
   config.filebrowserUploadMethod = 'form';
   config.toolbar_Pure = [
     '/', {
