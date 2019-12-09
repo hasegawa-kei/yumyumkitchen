105a51643df7766c0b3b610a9cefe47de6a8260e
hasegawa kei <yahooku.kei@gmail.com>
2019-12-09 15:57:37 +0900

    upload修正4

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module Yumyumkitchen
     config.i18n.default_locale = :ja # デフォルトのlocaleを日本語(:ja)にする
     #config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]　#日本語対応
     config.time_zone = 'Tokyo'
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
