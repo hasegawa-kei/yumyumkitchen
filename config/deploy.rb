b10e65fb04fc345175f521c1273f63e80aad70c1
hasegawa kei <yahooku.kei@gmail.com>
2020-02-18 14:43:23 +0900

    Add for deploy by capistrano-2

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.12.0"
 
 set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "https://github.com/hasegawa-kei/yumyumkitchen"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
