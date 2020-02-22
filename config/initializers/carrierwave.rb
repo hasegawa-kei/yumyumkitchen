31a2418207055e4ca97ea7ecc6d2e63f8ee0a24c
hasegawa kei <yahooku.kei@gmail.com>
2020-02-23 00:07:17 +0900

    database修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,12 +2,12 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     # Amazon S3用の設定
-    :provider              => 'AWS',
-    :region                => ENV['AWS_S3_REGION'],
-    :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    provider: 'AWS',
+    region: Rails.application.credentials.aws[:s3_region],
+    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key]
   }
-  config.fog_directory     =  ENV['AWS_S3_BUCKET']
+  config.fog_directory     =  Rails.application.credentials.aws[:s3_bucket]
   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   case Rails.env
     when 'production'
