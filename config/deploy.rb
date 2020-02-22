24c0d21d2034abc0ca133b37429e166cd74a1f09
hasegawa kei <yahooku.kei@gmail.com>
2020-02-23 03:35:10 +0900

    datab

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "3.4.0"
+lock "~> 3.12.0"
 
 set :application, "yumyumkitchen"
 set :repo_url, 'git@github.com:hasegawa-kei/yumyumkitchen.git'
