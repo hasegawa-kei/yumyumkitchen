4572bc65f4c18bc29ee4e57d5256da1ad918058d
hasegawa kei <yahooku.kei@gmail.com>
2020-02-23 03:18:38 +0900

    databas

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, 'git@github.com:hasegawa-kei/yumyumkitchen.git'
 set :branch, 'master'
 
 # Default deploy_to directory is /var/www/my_app_name
- set :deploy_to, "/var/www/yumyumkitchen"
+ set :deploy_to, "/var/www/rails/yumyumkitchen"
 
  # シンボリックリンクをはるファイル。
  set :linked_files, fetch(:linked_files, []).push('config/master.key')
