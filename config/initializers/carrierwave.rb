50da9121268f583242c25a4514df2564bf009c26
hasegawa kei <yahooku.kei@gmail.com>
2020-03-14 23:04:33 +0900

    carrierwave.rb編集

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,30 +1,30 @@
 # frozen_string_literal: true
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      # Amazon S3用の設定
+      provider: 'AWS',
+      region: Rails.application.credentials.aws[:s3_region],
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key]
+    }
+    config.fog_directory = Rails.application.credentials.aws[:s3_bucket]
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+    case Rails.env
+    when 'production'
+      config.fog_directory = 'railspotoforio'
+      config.asset_host = 'https://railspotoforio.s3-ap-northeast-1.amazonaws.com'
 
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    # Amazon S3用の設定
-    provider: 'AWS',
-    region: Rails.application.credentials.aws[:s3_region],
-    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key]
-  }
-  config.fog_directory = Rails.application.credentials.aws[:s3_bucket]
-  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
-  case Rails.env
-  when 'production'
-    config.fog_directory = 'railspotoforio'
-    config.asset_host = 'https://railspotoforio.s3-ap-northeast-1.amazonaws.com'
+    when 'development'
+      config.fog_directory = 'railspotoforio'
+      config.asset_host = 'https://railspotoforio.s3-ap-northeast-1.amazonaws.com'
 
-  when 'development'
-    config.fog_directory = 'railspotoforio'
-    config.asset_host = 'https://railspotoforio.s3-ap-northeast-1.amazonaws.com'
-
-  when 'test'
-    config.fog_directory = 'railspotoforio'
-    config.asset_host = 'https://railspotoforio.s3-ap-northeast-1.amazonaws.com'
+    when 'test'
+      config.fog_directory = 'railspotoforio'
+      config.asset_host = 'https://railspotoforio.s3-ap-northeast-1.amazonaws.com'
+    end
   end
 end
-
 # 日本語ファイル名の設定
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
